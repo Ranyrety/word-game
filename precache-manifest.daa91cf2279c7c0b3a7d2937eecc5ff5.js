485574899246230265926d0837a9b4aeb6b3dd27
Michał <mihau.ruminski@gmail.com>
2019-05-20 10:18:16 +0200

    Updates

---
+++ precache-manifest.daa91cf2279c7c0b3a7d2937eecc5ff5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e73c6b1365835ac6c2f3e0cfea1bd2ae",
+    "url": "/word-game/index.html"
+  },
+  {
+    "revision": "acab0df16e076a374d85",
+    "url": "/word-game/static/css/main.f8417313.chunk.css"
+  },
+  {
+    "revision": "bca943ab24bdfcd15706",
+    "url": "/word-game/static/js/2.f0927397.chunk.js"
+  },
+  {
+    "revision": "acab0df16e076a374d85",
+    "url": "/word-game/static/js/main.f2ada117.chunk.js"
+  },
+  {
+    "revision": "c7da4e5de2e49be43a10",
+    "url": "/word-game/static/js/runtime~main.464dc393.js"
+  }
+]);
