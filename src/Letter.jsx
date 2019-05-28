54105e3af2042113f72d70971b0f1c2c0a71b029
Michał <mihau.ruminski@gmail.com>
2019-05-28 13:35:02 +0200

    Changed Letter to function component

--- src/Letter.jsx
+++ src/Letter.jsx
@@ -1,32 +1,14 @@
 import React from 'react'
 import './Letter.css'
 
-class Letter extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      correctClicked: false,
-      incorrectClicked: false
-    }
-  }
-
-  static getDerivedStateFromProps(nextProps, prevState) {
-    if (prevState.correctClicked)
-      return null
-    else
-      if (nextProps.correctClicked !== prevState.correctClicked || nextProps.incorrectClicked !== prevState.incorrectClicked) {
-        return { correctClicked: nextProps.correctClicked, incorrectClicked: nextProps.incorrectClicked }
-      }
-    return null
-  }
 
 
-  render() {
+function Letter(props) {
     let bgColor = "#095567"
-    if (this.state.incorrectClicked) {
+    if (props.incorrectClicked) {
       bgColor = "#771010"
     }
-    else if (this.state.correctClicked) {
+    else if (props.correctClicked) {
       bgColor = "#395501"
     }
     let style = {
@@ -34,8 +16,8 @@ class Letter extends React.Component {
     };
 
     return (
-      <div className="letter" id={this.props.id} style={style} onClick={this.props.onClicked}>{this.props.letter}</div>
+      <div className="letter" id={props.id} style={style} onClick={props.onClicked}>{props.letter}</div>
     );
   }
-}
+
 export default Letter
