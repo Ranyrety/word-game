158fab2cc26a169ebc6829401fc3eff5752ce2fe
Michał <mihau.ruminski@gmail.com>
2019-06-18 16:13:41 +0200

    Changed MainMenu component to functional

--- src/MainMenu.jsx
+++ src/MainMenu.jsx
@@ -1,38 +1,30 @@
 import React from 'react'
 import Modal from './Modal';
 
-class MainMenu extends React.Component {
+function MainMenu (props) {
 
-  constructor(props) {
-    super(props)
-
-    this.handleClick = this.handleClick.bind(this)
+  const handleClick = (e) =>{
+    {
+      const id = e.target.id
+      props.onSetDifficulty(id)
+    }
   }
-
-
-  handleClick(e) {
-    const id = e.target.id
-    this.props.onSetDifficulty(id)
-  }
-
-  render() {
-    return (
-      <div>
-        <Modal show={this.props.difficultyVisible} handleClose={this.props.handleClose}>
-          <button id='0' onClick={this.handleClick}>Easy</button>
-          <button id='1' onClick={this.handleClick}>Medium</button>
-          <button id='2' onClick={this.handleClick}>Hard</button>
-        </Modal>
-        <ul>
-          <li>
-            <button onClick={this.props.onNewGame}>New Game</button>
-          </li>
-          <li>
-            <button onClick={this.props.showChoseDifficulty}>Difficulty settings</button>
-          </li>
-        </ul>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <Modal show={props.difficultyVisible} handleClose={props.handleClose}>
+        <button id='0' onClick={handleClick}>Easy</button>
+        <button id='1' onClick={handleClick}>Medium</button>
+        <button id='2' onClick={handleClick}>Hard</button>
+      </Modal>
+      <ul>
+        <li>
+          <button onClick={props.onNewGame}>New Game</button>
+        </li>
+        <li>
+          <button onClick={props.showChoseDifficulty}>Difficulty settings</button>
+        </li>
+      </ul>
+    </div>
+  )
 }
 export default MainMenu
